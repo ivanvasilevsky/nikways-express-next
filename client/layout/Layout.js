6bb13add0fe888c049bb4674b157abb0abd6db8d
Ivan Vasilevsky <ivanvasilevsky@Ivans-MacBook-Air.local>
2023-06-02 00:55:05 +0600

    make transition

--- client/layout/Layout.js
+++ client/layout/Layout.js
@@ -2,21 +2,62 @@ import Footer from "@/component/main/Footer"
 import HeadBlock from "@/component/main/HeadBlock"
 import Header from "@/component/main/Header"
 import Menu from "@/component/main/menu/Menu"
+import WaveTransSvg from "@/component/ui/WaveTransSvg"
+import { AnimatePresence, motion } from "framer-motion"
+import { Router, useRouter } from "next/router"
+import { useState } from "react"
 
 export default function Layout({ children }) {
+
+  const [transitionActive, setTransitionActive] = useState(false)
+
+  Router.events.on('routeChangeStart', () => {
+    setTransitionActive(true)
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    setTransitionActive(false)
+  })
+
+  const router = useRouter()
+  const pageKey = router.asPath.split('/').reverse()[0].split('?')[0]
+
   return (
     <>
       <HeadBlock title={children.props.title}/>
 
       <div id="modal"></div>
 
+      <AnimatePresence mode="wait">
+      {transitionActive &&
+        <motion.div
+          animate={{ top: 0 }}
+          exit={{ opacity: 0, transition: { delay: .9 }}}
+          transition={{ duration: 1 }}
+          className='transition'
+        >
+          <div className="transition__inner">
+            {transitionActive &&
+              <WaveTransSvg />
+            }
+            {!transitionActive &&
+              <WaveTransSvg />
+            }
+          </div>
+        </motion.div>
+      }
+      </AnimatePresence>
+
       <div className='page'>
 
-        <Menu/>
-        <Header/>
-
-        {children}
-        <Footer info={children.props.contacts}/>
+        <AnimatePresence mode="wait">
+          <motion.div key={pageKey} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: .1, delay: .30  }}>
+            <Menu/>
+            <Header/>
+            {children}
+            <Footer info={children.props.contacts}/>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </>
   )
